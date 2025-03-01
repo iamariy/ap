ce514586adcd33b875916e7db3ab835105a23912
user <mariiy1384@gmail.com>
2025-03-02 00:26:20 +0330

    Potential Energy

---
+++ Energy.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public  class Energy {
+    public static void main(String[] args) {
+        final float g = 9.8f;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter mass:");
+        float m = scanner.nextFloat();
+
+        System.out.print("enter height:");
+        float h = scanner.nextFloat();
+
+        float U = m*g*h;
+
+        System.out.println("Potential Energy :" + U);
+    }
+}
