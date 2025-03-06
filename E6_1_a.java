afc6dc2c80e409e76682e6e50b243e3336ccb883
user <mariiy1384@gmail.com>
2025-03-06 21:09:30 +0330

    Practice E6.1_a

---
+++ E6_1_a.java
@@ -0,0 +1,12 @@
+public class E6_1_a {
+    public static void main(String[] args)
+    {
+        int i=2,n=100,sum=0;
+
+        for (i=2;i<=n;i+=2)
+        {
+            sum+=i;
+        }
+        System.out.println("Sum is:"+ sum);
+    }
+}
