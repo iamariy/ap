e76116a34fdb045862706f1e108c17d983139d42
user <mariiy1384@gmail.com>
2025-03-06 21:49:18 +0330

    Practice E6.2_a

---
+++ E6_2_a.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class E6_2_a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter size of array");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        int i;
+        for (i = 0; i < n; i++) {
+            System.out.println("enter array elements");
+            arr[i] = scanner.nextInt();
+        }
+
+        int max = arr[0], min = arr[0];
+        for (i = 0; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        System.out.println("max is"+ max);
+        System.out.println("min is"+ min);
+    }
+}
