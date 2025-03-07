a51f493c85a05150f7ec423e943a8396fabb077d
user <mariiy1384@gmail.com>
2025-03-07 20:21:26 +0330

    Practice E6.16

---
+++ E6_16.java
@@ -0,0 +1,12 @@
+public class E6_16 {
+    public static void main(String[] args)
+    {
+        int x=10,y=10;
+        for (int i=1;i<=x;i++){
+            for (int j=1;j<=y;j++){
+                System.out.print(i*j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
