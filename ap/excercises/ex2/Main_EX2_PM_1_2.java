5dcc4498f24e1d09a97759c96949d0db3f08b387
user <mariiy1384@gmail.com>
2025-03-25 16:23:05 +0330

    EX2_PM_1_2

---
+++ ap/excercises/ex2/Main_EX2_PM_1_2.java
@@ -0,0 +1,30 @@
+package ap.excercises.ex2;
+
+import java.util.Scanner;
+
+public class Main_EX2_PM_1_2 {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter k");
+        int k=scanner.nextInt();
+
+        char[][] square=new char[k+2][k+2];
+        for (int i=0;i<k+2;i++)
+        {
+            for (int j=0;j<k+2;j++)
+            {
+                if (i==0 || i==k+1 || j==0 || j==k+1) square[i][j]='*';
+                else square[i][j]=' ';
+            }
+        }
+        for (int i=0;i<k+2;i++)
+        {
+            for (int j=0;j<k+2;j++)
+            {
+                System.out.print(square[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
