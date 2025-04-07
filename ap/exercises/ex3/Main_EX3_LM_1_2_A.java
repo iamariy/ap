2911d26565f098597e9e2e11340ce3c714c5e9bd
user <mariiy1384@gmail.com>
2025-04-08 00:40:11 +0330

    EX3_LM_1_2_A

--- ap/exercises/ex3/Main_EX3_LM_1_2_A.java
+++ ap/exercises/ex3/Main_EX3_LM_1_2_A.java
@@ -7,83 +7,55 @@ import java.io.IOException;
 
 public class Main_EX3_LM_1_2_A {
     public static void  main(String[] args) {
-        String str1=CreatingFileB();
-        String str2=CreatingFileS();
 
-        Book[] books=new Book[4];
-        books[0]=new Book("Cosette","Victor Hugo",582,1862);
-        books[1]=new Book("Anna Shirley","Lucy Maund Montgomery",3544,1908);
-        books[2]=new Book("Le Petir Prince","Antoine de Saint-Exupery",103,1944);
-        books[3]=new Book("The Forty Rules of Love","Elif Safak",511,2010);
-        for (int i=0;i< books.length;i++)
-        {
-            System.out.println(books[i].toString());
+        Book[] books = new Book[4];
+        books[0] = new Book("Cosette", "Victor Hugo", 582, 1862);
+        books[1] = new Book("Anna Shirley", "Lucy Maund Montgomery", 3544, 1908);
+        books[2] = new Book("Le Petir Prince", "Antoine de Saint-Exupery", 103, 1944);
+        books[3] = new Book("The Forty Rules of Love", "Elif Safak", 511, 2010);
 
-        }
+        Main_EX3_LM1_2_C.Student[] students= new Main_EX3_LM1_2_C.Student[3];
+        students[0]= new Main_EX3_LM1_2_C.Student("Maryam","Rezaei","Computer",763435);
+        students[1]= new Main_EX3_LM1_2_C.Student("Sama","Zolfkhani","Computer",478432);
+        students[2]= new Main_EX3_LM1_2_C.Student("Neda","Rashtchi","Computer",964532);
 
-        Student[] students=new Student[3];
-        students[0]=new Student("Maryam","Rezaei","Computer",763435);
-        students[1]=new Student("Sama","Zolfkhani","Computer",478432);
-        students[2]=new Student("Neda","Rashtchi","Computer",964532);
-        for (int j=0;j< books.length;j++)
-        {
-            System.out.println(students[j].toString());
-        }
 
-        WriteToFileB(books,str1);
-        WriteToFileS(students,str2);
-    }
 
-    public static String CreatingFileB()
-    {
-        String filepath = "Book.txt";
-        File file=new File(filepath);
+        String filepath1 = "Book.txt";
+        File file1 = new File(filepath1);
 
-        if (!file.exists()) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+        if (!file1.exists()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath1))) {
                 System.out.println("Created the fileB.");
             } catch (IOException e) {
                 System.out.println("Error to creating file!");
                 e.printStackTrace();
             }
         } else System.out.println("Created the file before.");
-        return filepath;
-    }
-    public static void WriteToFileB(Book[] books, String filepath1)
-    {
-        try (BufferedWriter writer=new BufferedWriter(new FileWriter(filepath1)))
-        {
-            for (int i=0;i< books.length;i++)
-            {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath1))) {
+            for (int i = 0; i < books.length; i++) {
                 writer.write(books[i].toString());
                 writer.newLine();
             }
             System.out.println("Book saved successfully!");
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Error to saving file.");
             e.printStackTrace();
         }
-    }
 
-    public static String CreatingFileS()
-    {
-        String filepath = "Student.txt";
-        File file=new File(filepath);
+        String filepath2 = "Student.txt";
+        File file2 = new File(filepath2);
 
-        if (!file.exists()) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+        if (!file2.exists()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath2))) {
                 System.out.println("Created the file.");
             } catch (IOException e) {
                 System.out.println("Error to creating file!");
                 e.printStackTrace();
             }
         } else System.out.println("Created the file before.");
-        return filepath;
-    }
-    public static void WriteToFileS(Student[] students, String filepath2 )
-    {
+
         try (BufferedWriter writer=new BufferedWriter(new FileWriter(filepath2)))
         {
             for (int j=0;j< students.length;j++)
@@ -99,6 +71,7 @@ public class Main_EX3_LM_1_2_A {
             e.printStackTrace();
         }
     }
+
     static class Book
     {
         String bookname;
@@ -136,7 +109,7 @@ public class Main_EX3_LM_1_2_A {
             this.year = year;
         }
     }
-    static class Student {
+    class Student {
         String firstname;
         String lastname;
         String major;
