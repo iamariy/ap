fcdab9d2405b77eea1ab323fec340374236dc7c8
user <mariiy1384@gmail.com>
2025-04-08 00:40:37 +0330

    EX3_LM_1_2_B

--- ap/exercises/ex3/Main_EX3_LM_1_2_B.java
+++ ap/exercises/ex3/Main_EX3_LM_1_2_B.java
@@ -1,71 +1,46 @@
 package ap.exercises.ex3;
 
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main_EX3_LM_1_2_B {
     public static void  main(String[] args) {
-        String str1=CreatingFileB();
-        String str2=CreatingFileS();
 
-        Book[] books=new Book[4];
-        books[0]= new Book("Cosette","Victor Hugo",582,1862);
-        books[1]= new Book("Anna Shirley","Lucy Maund Montgomery",3544,1908);
-        books[2]= new Book("Le Petir Prince","Antoine de Saint-Exupery",103,1944);
-        books[3]= new Book("The Forty Rules of Love","Elif Safak",511,2010);
+        Main_EX3_LM1_2_C.Book[] books=new Main_EX3_LM1_2_C.Book[4];
+        books[0]= new Main_EX3_LM1_2_C.Book("Cosette","Victor Hugo",582,1862);
+        books[1]= new Main_EX3_LM1_2_C.Book("Anna Shirley","Lucy Maund Montgomery",3544,1908);
+        books[2]= new Main_EX3_LM1_2_C.Book("Le Petir Prince","Antoine de Saint-Exupery",103,1944);
+        books[3]= new Main_EX3_LM1_2_C.Book("The Forty Rules of Love","Elif Safak",511,2010);
 
-        Student[] students= new Student[3];
-        students[0]= new Student("Maryam","Rezaei","Computer",763435);
-        students[1]= new Student("Sama","Zolfkhani","Computer",478432);
-        students[2]= new Student("Neda","Rashtchi","Computer",964532);
-
-        WriteToFileB(books,str1);
-        WriteToFileS(students,str2);
-        ReadToFileB(books,str1);
-        ReadToFileS(students,str2);
-    }
+        Main_EX3_LM1_2_C.Student[] students= new Main_EX3_LM1_2_C.Student[3];
+        students[0]= new Main_EX3_LM1_2_C.Student("Maryam","Rezaei","Computer",763435);
+        students[1]= new Main_EX3_LM1_2_C.Student("Sama","Zolfkhani","Computer",478432);
+        students[2]= new Main_EX3_LM1_2_C.Student("Neda","Rashtchi","Computer",964532);
 
 
-    public static String CreatingFileB()
-    {
-        String filepath = "Book.txt";
-        File file=new File(filepath);
+        String filepath1 = "Book.txt";
+        File file1 = new File(filepath1);
 
-        if (!file.exists()) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+        if (!file1.exists()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath1))) {
                 System.out.println("Created the fileB.");
             } catch (IOException e) {
                 System.out.println("Error to creating file!");
                 e.printStackTrace();
             }
         } else System.out.println("Created the file before.");
-        return filepath;
-    }
-    public static void WriteToFileB(Book[] books, String filepath1)
-    {
-        try (BufferedWriter writer=new BufferedWriter(new FileWriter(filepath1)))
-        {
-            for (int i=0;i< books.length;i++)
-            {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath1))) {
+            for (int i = 0; i < books.length; i++) {
                 writer.write(books[i].toString());
                 writer.newLine();
             }
             System.out.println("Book saved successfully!");
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Error to saving file.");
             e.printStackTrace();
         }
-    }
 
-    public static void ReadToFileB(Book[] books, String filepath)
-    {
-        try(BufferedReader reader=new BufferedReader(new FileReader(filepath)))
+        try(BufferedReader reader=new BufferedReader(new FileReader(filepath1)))
         {
             String line;
             while ((line= reader.readLine())!=null)
@@ -78,25 +53,19 @@ public class Main_EX3_LM_1_2_B {
             System.out.println("Error to reading file!"+ e.getMessage());
             e.printStackTrace();
         }
-    }
 
-    public static String CreatingFileS()
-    {
-        String filepath = "Student.txt";
-        File file=new File(filepath);
+        String filepath2 = "Student.txt";
+        File file2 = new File(filepath2);
 
-        if (!file.exists()) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+        if (!file2.exists()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath2))) {
                 System.out.println("Created the file.");
             } catch (IOException e) {
                 System.out.println("Error to creating file!");
                 e.printStackTrace();
             }
         } else System.out.println("Created the file before.");
-        return filepath;
-    }
-    public static void WriteToFileS(Student[] students, String filepath2 )
-    {
+
         try (BufferedWriter writer=new BufferedWriter(new FileWriter(filepath2)))
         {
             for (int j=0;j< students.length;j++)
@@ -111,11 +80,8 @@ public class Main_EX3_LM_1_2_B {
             System.out.println("Error to saving file.");
             e.printStackTrace();
         }
-    }
 
-    public static void ReadToFileS(Student[] students, String filepath)
-    {
-        try(BufferedReader reader=new BufferedReader(new FileReader(filepath)))
+        try(BufferedReader reader=new BufferedReader(new FileReader(filepath2)))
         {
             String line;
             while ((line= reader.readLine())!=null)
@@ -130,7 +96,7 @@ public class Main_EX3_LM_1_2_B {
         }
     }
 
-    static class Book
+    class Book
     {
         String bookname;
         String authornam;
@@ -167,7 +133,7 @@ public class Main_EX3_LM_1_2_B {
             this.year = year;
         }
     }
-    static class Student {
+    class Student {
         String firstname;
         String lastname;
         String major;
@@ -180,9 +146,8 @@ public class Main_EX3_LM_1_2_B {
             this.id = id;
         }
 
-        public String toString()
-        {
-            return firstname +","+ lastname +","+ major +","+ id;
+        public String toString() {
+            return firstname + "," + lastname + "," + major + "," + id;
         }
 
         public void setFirstName(String firstName) {
