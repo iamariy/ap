a380227c15e54f9627d95e2e679d9e8edbce3944
user <mariiy1384@gmail.com>
2025-04-28 22:18:39 +0330

    E3.4

---
+++ ap/exercises/ex4/E3_4.java
@@ -0,0 +1,54 @@
+package ap.exercises.ex4;
+
+public class E3_4 {
+
+        private int switch1;
+        private int switch2;
+        private int lamp;
+
+        public E3_4()
+        {
+            this.switch1=0;
+            this.switch2=0;
+        }
+        public int getFirstSwitchState()
+        {
+            return switch1;
+        }
+        public int getSecondSwitchState()
+        {
+            return switch2;
+        }
+        public int getLampState()
+        {
+            return lamp;
+        }
+        public void toggleFirstSwitch()
+        {
+            switch1=1-switch1;
+            updating();
+        }
+        public void toggleSecondSwitch()
+        {
+            switch2=1-switch2;
+            updating();
+        }
+        public void updating()
+        {
+            lamp=switch1^switch2;
+        }
+    public static void main(String[] args)
+    {
+        E3_4 light=new E3_4();
+        print(light);
+        light.toggleFirstSwitch();
+        print(light);
+        light.toggleSecondSwitch();
+        print(light);
+
+    }
+        public static void print(E3_4 light)
+        {
+            System.out.println("First Switch:"+ light.getFirstSwitchState()+"\nSecond Switch:"+ light.getSecondSwitchState() +"\nLamp:"+ (light.getLampState()==1?"on":"off"));
+        }
+    }
