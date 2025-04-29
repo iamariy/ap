93a54f63895d9be8000a58caa639bc47e453f7e1
user <mariiy1384@gmail.com>
2025-04-29 21:20:03 +0330

    E3.6

---
+++ ap/exercises/ex4/E3_6.java
@@ -0,0 +1,34 @@
+package ap.exercises.ex4;
+
+public class E3_6 {
+    private int[] switches;
+    private int lamp;
+
+    public E3_6(){
+        switches=new int[2];
+        updatelamp();
+    }
+    public int getSwitchState(int switchNum){
+        if (switchNum==0 || switchNum==1){
+            return switches[switchNum];
+        } else {
+            System.out.println("Invalid switch number.");
+            return -1;
+        }
+    }
+    public int getLampState(){
+        return lamp;
+    }
+    public void toggleSwitch(int switchNum){
+        if (switchNum==0 || switchNum==1){
+            int index=switchNum;
+            switches[index]=1-switches[index];
+            updatelamp();
+        } else {
+            System.out.println("Invalid switch number.");
+        }
+    }
+    public void updatelamp(){
+        lamp=switches[0]^switches[1];
+    }
+}
